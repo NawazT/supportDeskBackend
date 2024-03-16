dd90ee12ce5127321efba7ef34918a899c053d49
NawazT <nawaztausif63@gmail.com>
2024-03-16 16:31:06 +0530

    modified

--- server.js
+++ server.js
@@ -23,16 +23,16 @@ app.use(express.urlencoded({extended : false}))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/tickets', require('./routes/ticketRoutes'))
 
-// Serve Frontend
-if (process.env.NODE_ENV === 'production') {
-    // Set build folder as static
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
-}else{
-    app.get('/',(req,res) => {
-        res.status(200).json({message: 'Welcome to the support desk API'})
-    })
-}
+// // Serve Frontend
+// if (process.env.NODE_ENV === 'production') {
+//     // Set build folder as static
+//     app.use(express.static(path.join(__dirname, '../frontend/build')))
+//     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+// }else{
+//     app.get('/',(req,res) => {
+//         res.status(200).json({message: 'Welcome to the support desk API'})
+//     })
+// }
 
 //error Handler
 app.use(errorHandler)
